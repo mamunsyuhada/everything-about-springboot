ef0a2f3a5ce8e1154b4fb0205e49e3edbbdefe4d
Imam Syuhada <imamsy11@gmail.com>
2022-10-10 04:45:31 +0700

    feat: student model & controller

--- spring-pgsql-jpa/src/main/java/com/springjpa/demo/DemoApplication.java
+++ spring-pgsql-jpa/src/main/java/com/springjpa/demo/DemoApplication.java
@@ -1,34 +1,12 @@
 package com.springjpa.demo;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.springjpa.demo.student.Student;
 
 @SpringBootApplication
-@RestController()
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping()
-	public List<Student> helloWorld() {
-		return List.of(
-			new Student(
-				1L, 
-				"Mamun",
-				"imam@learnspring.boot", 
-				LocalDate.of(2000, Month.APRIL, 5),
-				21
-			)
-		);
 	}
 }
--- spring-pgsql-jpa/src/main/java/com/springjpa/demo/student/Student.java
+++ spring-pgsql-jpa/src/main/java/com/springjpa/demo/student/Student.java
@@ -8,12 +8,10 @@ public class Student {
   private String email;
   private LocalDate dob;
   private Integer age;
-
   
   public Student() {
   }
 
-  
   public Student(Long id, String name, String email, LocalDate dob, Integer age) {
     this.id = id;
     this.name = name;

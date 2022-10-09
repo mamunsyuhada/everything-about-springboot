81b16349d924e109121f58137dd0c2096bbc26c6
Imam Syuhada <imamsy11@gmail.com>
2022-10-10 04:13:58 +0700

    feat: create student model & prove it to api

--- spring-pgsql-jpa/src/main/java/com/springjpa/demo/DemoApplication.java
+++ spring-pgsql-jpa/src/main/java/com/springjpa/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.springjpa.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springjpa.demo.student.Student;
 
 @SpringBootApplication
 @RestController()
@@ -16,7 +20,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping()
-	public List<String> helloWorld() {
-		return List.of("Test", "Hello");
+	public List<Student> helloWorld() {
+		return List.of(
+			new Student(
+				1L, 
+				"Mamun",
+				"imam@learnspring.boot", 
+				LocalDate.of(2000, Month.APRIL, 5),
+				21
+			)
+		);
 	}
 }
